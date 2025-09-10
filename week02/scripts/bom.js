3956d929c29c6a4901e9e2ff0e689ef069e699bb
sebasss18 <sebasbernals11@gmail.com>
2025-09-10 11:40:03 -0700

    learning activities

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,12 +1,26 @@
 const input = (document.querySelector('#favchap'));
 const button = (document.querySelector('button'));
-const list = (document.querySelector('list'));
-const li = (document.createElement('li'));
-const deleteButton = (document.createElement ('button'));
+const list = (document.querySelector('#list'));
 
-li.textContent = input.value;
+button.addEventListener('click', function() {
 
-deleteButton.textContent = ('❌');
+    if (input.value.trim() !== '') {
+        const li = (document.createElement('li'));
+        const deleteButton = (document.createElement ('button'));
 
-li.append(deleteButton);
-li.append(li);
+        li.textContent = input.value;
+
+        deleteButton.textContent = ('❌');
+
+        deleteButton.addEventListener('click', function(){
+            list.removeChild(li);
+            input.focus();
+        })
+
+        li.append(deleteButton);
+        list.appendChild(li);
+
+        input.focus();
+        input.value = '';
+    }
+})
